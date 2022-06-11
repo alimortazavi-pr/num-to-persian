7acfc61d08e13a28c38244a6cad7775e7d390724
Ali Mortazavi <mortazavimaneshali@gmail.com>
2022-06-11 22:16:03 +0430

    V1

---
+++ src/indexx.ts
@@ -0,0 +1,12 @@
+//Convert numbers
+function convertToPersian(number: number): string {
+  return number
+    .toString()
+    .replace(
+      /\d/g,
+      (x: string | number) =>
+        ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"][x as number]
+    );
+}
+
+export default convertToPersian;
